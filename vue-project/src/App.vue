250f927bfb786af31721870049b829af7f8585ea
Fiona Hu <fionahufeifei@126.com>
2024-11-17 22:09:50 -0600

    1-5 done

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-waether-primary">
+    <SiteNavigation />
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import { Routview } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
 </script>
 
 <style lang="scss" scoped>
