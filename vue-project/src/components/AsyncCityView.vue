27e261f8ff77b94c1c2fb73a991f5b6d1b3b1c93
Fiona Hu <fionahufeifei@126.com>
2024-11-19 17:54:57 -0600

    step8 5:20

---
+++ vue-project/src/components/AsyncCityView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const getWeatherData = async() => {
+    try {
+        const weatherData = await axios.get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=ffb4dfa520bea4a7eb096da1e21ceea0&units=imperial`
+        );
+    } catch(err) {
+
+    }
+};
+</script>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,20 +1,43 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
+import { useRoute } from 'vue-router';
+import { preview } from 'vite';
+
+const router = useRoute();
+const preViewCity = (searchResult) => {
+  console.log(searchResult);
+  const [city, state] = searchResult.place_name.split(",");
+  // console.log(city, state);
+  router.push({
+    name: "cityView",
+    params: { state: state.replaceAll(" ", ""), city: city},
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
+};
 
 const mapboxAPIKey = "pk.eyJ1IjoiZmlvbmFodTIiLCJhIjoiY20zbWd0MXFjMDBjcTJpbXpqaWk1c3R1MSJ9.3iI3b6oKxO5rG_E5OkkKmg";
 const mapboxSearchResults = ref(null);
 const searchQuery = ref("");
 const queryTimeout = ref(null);
+const searchError = ref(null);
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async() => {
     if (searchQuery.value != "") {
-      const result = await axios.get(
+      try {
+        const result = await axios.get(
         `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
-      );
-      mapboxSearchResults.value = result.data.features;
-      console.log(mapboxSearchResults.value);
+        );
+        mapboxSearchResults.value = result.data.features;
+        console.log(mapboxSearchResults.value);
+      } catch {
+        searchError = true;
+      }
       return;
     }
     mapboxSearchResults.value = null;
@@ -29,9 +52,18 @@ const getSearchResults = () => {
       placeholder="Search for a city of state" 
       class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
       <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]" v-if="mapboxSearchResults">
-        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
-          {{ searchResult.place_name }}
-        </li>
+        <p v-if="searchError">
+          Sorry, something went wrong, please try again.
+        </p>
+        <p v-if="!searchError && mapboxSearchResults.length === 0">
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer"
+          @click="preViewCity(searchResult)">
+            {{ searchResult.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
